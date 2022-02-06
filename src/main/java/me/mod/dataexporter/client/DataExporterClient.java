72951d6e4f2c9a9f19eda3408dadcd317b45c8f5
Janis Pritzkau <janispritzkau@gmail.com>
2022-02-06 15:11:13 +0100

    Initial commit

---
+++ src/main/java/me/mod/dataexporter/client/DataExporterClient.java
@@ -0,0 +1,13 @@
+package me.mod.dataexporter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class DataExporterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
